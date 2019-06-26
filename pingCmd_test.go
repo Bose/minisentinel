13df725256da6bcb5df5834db3d5f0504401ac28
Jim Lambert <jim_lambert@bose.com>
2019-06-26 07:50:58 -0400

    ping command

---
+++ pingCmd_test.go
@@ -0,0 +1,30 @@
+package minisentinel
+
+import (
+	"testing"
+
+	"github.com/alicebob/miniredis"
+	"github.com/matryer/is"
+
+	"github.com/gomodule/redigo/redis"
+)
+
+func TestPing(t *testing.T) {
+	is := is.New(t)
+
+	m, err := miniredis.Run()
+	is.NoErr(err)
+	s, err := Run(m)
+	is.NoErr(err)
+	defer s.Close()
+	c, err := redis.Dial("tcp", s.Addr())
+	is.NoErr(err)
+
+	// PING command
+	{
+		v, err := redis.String(c.Do("PING"))
+		t.Logf("PING returned: %v", v)
+		is.NoErr(err)
+		is.True(v == "PONG")
+	}
+}
