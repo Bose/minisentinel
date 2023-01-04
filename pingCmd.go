645138d1d8d77eabf7c60326403f23bad2406803
刘顺钰 <shunyu.liu@chaitin.com>
2023-01-04 18:00:05 +0800

    fix: fix bug of `NewSentinel`,`Run`,`handleAuth`

--- pingCmd.go
+++ pingCmd.go
@@ -4,6 +4,7 @@ import "github.com/alicebob/miniredis/v2/server"
 
 func commandsPing(s *Sentinel) {
 	s.srv.Register("PING", s.cmdPing)
+	s.srv.Register("AUTH", s.cmdAuth)
 }
 
 // cmdPing
@@ -16,4 +17,27 @@ func (s *Sentinel) cmdPing(c *server.Peer, cmd string, args []string) {
 		return
 	}
 	c.WriteInline("PONG")
+}
+
+func (s *Sentinel) cmdAuth(c *server.Peer, cmd string, args []string) {
+	if len(args) != 1 {
+		c.WriteError(errWrongNumber(cmd))
+		return
+	}
+
+	pw := args[0]
+
+	s.Lock()
+	defer s.Unlock()
+	if s.password == "" {
+		c.WriteError("ERR Client sent AUTH, but no password is set")
+		return
+	}
+	if s.password != pw {
+		c.WriteError("ERR invalid password")
+		return
+	}
+
+	setAuthenticated(c)
+	c.WriteOK()
 }
